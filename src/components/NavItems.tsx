0c6498cdad458348ee6c7993d593c008f90447b5
Femosky <olufemiojeyemi@gmail.com>
2023-11-24 12:01:39 +0100

    1:14:58

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,9 +1,37 @@
 'use client';
 
+import { PRODUCT_CATEGORIES } from '@/config';
 import { useState } from 'react';
+import { NavItem } from './NavItem';
 
 export function NavItems() {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    return <div className="flex gap-4 h-full"></div>;
+    const isAnyOpen = activeIndex !== null;
+
+    return (
+        <div className="flex gap-4 h-full">
+            {PRODUCT_CATEGORIES.map((category, i) => {
+                function handleOpen() {
+                    if (activeIndex === i) {
+                        setActiveIndex(null);
+                    } else {
+                        setActiveIndex(i);
+                    }
+                }
+
+                const isOpen = i === activeIndex;
+
+                return (
+                    <NavItem
+                        key={category.value}
+                        category={category}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        isAnyOpen={isAnyOpen}
+                    />
+                );
+            })}
+        </div>
+    );
 }
